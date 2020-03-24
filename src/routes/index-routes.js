65d0bea95b67967e211623d96b323389a1ecb874
Bianca Pereira <bianca.almeida@grupolpj.com>
2020-03-23 23:27:53 -0300

    salvando dados no arquivo

--- src/routes/index-routes.js
+++ src/routes/index-routes.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const fs = require('fs');
+const path = require('path');
+const request = require('request');
 const router = express.Router();
+
 function criptografa(teste, numCaracter) {
   var texto = [];
   for (i = 0; i < teste.length; i++) {
@@ -14,6 +18,31 @@ router.get("/", (req, res, next) => {
   res.status(200).send({
     title: criptografa(teste, 3)
   });
+});
+
+router.get("/file", (req, res, next) => {
+  let pathJson = path.resolve(__dirname, 'answer.json');
+  var result;
+  request('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=SEU_TOKEN', (error, response, body) => {
+    if (error) {
+      console.error('error:', error);
+    }
+    console.log('status:', response && response.statusCode) 
+    result = body
+    console.log(result)
+    fs.writeFile(pathJson, result, err => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('File was saved')
+      }
+      
+    });
+    res.status(200).send({
+      result
+    });
+  });
+
 });
 
 module.exports = router;
