5cbd6ed7bf533dd5a27adaabcd8c9c52b32e079f
Brenner Fagundes <bhfagundes@gmail.com>
2020-03-23 22:58:37 -0300

    funcao de criptografar

--- src/routes/index-routes.js
+++ src/routes/index-routes.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
+function criptografa(teste, numCaracter) {
+  var texto = [];
+  for (i = 0; i < teste.length; i++) {
+    texto[i] = String.fromCharCode(teste.charCodeAt(i) + numCaracter);
+  }
+  return texto.join("");
+}
 
 router.get("/", (req, res, next) => {
   var teste = "help";
 
   res.status(200).send({
-    title: teste[0].charCodeAt() + 1
+    title: criptografa(teste, 3)
   });
 });
 
