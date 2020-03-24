2721dbf06bfe47a4f55f79af4b7f796a280a4535
Brenner Fagundes <bhfagundes@gmail.com>
2020-03-23 22:37:42 -0300

    primeiro teste asc

--- src/routes/index-routes.js
+++ src/routes/index-routes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+  var teste = "help";
+
   res.status(200).send({
-    title: "MentionsAPI",
-    version: "1.0.0"
+    title: teste[0].charCodeAt() + 1
   });
 });
 
